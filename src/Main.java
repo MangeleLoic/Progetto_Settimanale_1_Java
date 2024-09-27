4a0cff490c714b80ef562dc9077d8abb5cb0b630
sudonona <80674485+sudonona@users.noreply.github.com>
2024-09-27 13:30:42 +0200

    adds

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        RegistrazioneAudio audio1 = new RegistrazioneAudio("Traccia 1", 5, 7);
 
-        RegistrazioneAudio audio2 = new RegistrazioneAudio("Traccia 2", 3);
+        System.out.println("Inserisci il titolo per 'Traccia 1': ");
+        String titoloAudio1 = scanner.nextLine();
 
-        System.out.println("Sta riproducendo: ");
+        System.out.println("Inserisci la durata per 'Traccia 1': ");
+        int durataAudio1 = scanner.nextInt();
+
+        System.out.println("Inserisci il volume per 'Traccia 1': ");
+        int volumeAudio1 = scanner.nextInt();
+
+        RegistrazioneAudio audio1 = new RegistrazioneAudio(titoloAudio1, durataAudio1, volumeAudio1);
+
+
+        System.out.println("Sta riproducendo 'Traccia 1':");
         audio1.play();
 
-        System.out.println("Sta riproducendo::");
+
+        scanner.nextLine();
+        System.out.println("Inserisci il titolo per 'Traccia 2': ");
+        String titoloAudio2 = scanner.nextLine();
+
+        System.out.println("Inserisci la durata per 'Traccia 2': ");
+        int durataAudio2 = scanner.nextInt();
+
+        System.out.println("Inserisci il volume per 'Traccia 2': ");
+        int volumeAudio2 = scanner.nextInt();
+
+
+        RegistrazioneAudio audio2 = new RegistrazioneAudio(titoloAudio2, durataAudio2, volumeAudio2);
+
+
+        System.out.println("Sta riproducendo 'Traccia 2' :");
         audio2.play();
 
 
+        scanner.nextLine();
+        System.out.println("Inserisci il titolo del video:");
+        String titoloVideo = scanner.nextLine();
 
+        System.out.println("Inserisci la durata del video:");
+        int durataVideo = scanner.nextInt();
+
+        System.out.println("Inserisci il volume del video:");
+        int volumeVideo = scanner.nextInt();
+
+        System.out.println("Inserisci la luminosità del video:");
+        int luminositaVideo = scanner.nextInt();
+
+
+        Video video = new Video(titoloVideo, durataVideo, volumeVideo, luminositaVideo);
+
+
+        System.out.println("Sta riproducendo il video:");
+        video.play();
+
+
+        scanner.close();
     }
 }
--- src/Video.java
+++ src/Video.java
@@ -1,10 +1,45 @@
-public class Video extends ElementoMultimediale{
+public class Video extends ElementoMultimediale {
+    private int volume;
+    private int luminosita;
+
+    public Video(String titolo, int durata, int volume, int luminosita) {
+        super(titolo, durata);
+        this.volume = volume;
+        this.luminosita = luminosita;
+    }
+
     public Video(String titolo, int durata) {
         super(titolo, durata);
+        this.volume = 5;
+        this.luminosita = 5;
     }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
+    public int getLuminosita() {
+        return luminosita;
+    }
+
+    public void setLuminosita(int luminosita) {
+        this.luminosita = luminosita;
+    }
+
 
     @Override
     public void play() {
+        String titolo = super.getTitolo();
+        int durata = super.getDurata();
 
+        for (int i = 0; i < durata; i++) {
+            System.out.println(titolo + " " + "!".repeat(volume) + " " + "*".repeat(luminosita));
+        }
     }
 }
